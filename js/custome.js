b9bb0c67463484b33e9957af204b8103a2857b3f
solaymanWeb <solaymanweb27@gmail.com>
2022-11-21 21:22:42 +0600

    some update

---
+++ js/custome.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.client-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:".prev",
+        nextArrow:".next",
+    });
+});
